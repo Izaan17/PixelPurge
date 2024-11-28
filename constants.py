efe69a1126fef2441f3bd59ae24ac63b064305d0
izaan.noman <izaan.nyc@gmail.com>
2024-11-27 23:08:42 -0500

    Add IS_MAC variable

--- constants.py
+++ constants.py
@@ -4,3 +4,4 @@ HOME_PATH = os.path.expanduser('~')
 PIXEL_PURGE_FOLDER = os.path.join(HOME_PATH, 'PixelPurge')
 PIXEL_PURGE_DIRECTORIES_FILE = os.path.join(PIXEL_PURGE_FOLDER, 'directories.json')
 DEFAULT_DIRECTORIES = [os.path.join(HOME_PATH, 'AppData')]
+IS_MAC = os.uname().sysname == 'Darwin'
