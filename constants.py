8e1d97f4db14ecd513459f7f31ca975d41deeba0
izaan noman <izaan.nyc@gmail.com>
2024-11-28 01:22:00 -0500

    Fix platform error

--- constants.py
+++ constants.py
@@ -5,4 +5,4 @@ HOME_PATH = os.path.expanduser('~')
 PIXEL_PURGE_FOLDER = os.path.join(HOME_PATH, 'PixelPurge')
 PIXEL_PURGE_DIRECTORIES_FILE = os.path.join(PIXEL_PURGE_FOLDER, 'directories.json')
 DEFAULT_DIRECTORIES = [os.path.join(HOME_PATH, 'AppData')]
-IS_MAC = platform.system() == 'posix'
+IS_MAC = platform.system() == 'Darwin'
