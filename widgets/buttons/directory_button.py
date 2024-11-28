27bd37588552695d14d94725d4bee28d117d4c14
izaan.noman <izaan.nyc@gmail.com>
2024-11-27 23:45:39 -0500

    add light mode colors

--- widgets/buttons/directory_button.py
+++ widgets/buttons/directory_button.py
@@ -3,5 +3,5 @@ import customtkinter
 class DirectoryButton(customtkinter.CTkButton):
     def __init__(self, master: any, **kwargs):
         super().__init__(master, **kwargs)
-        self.configure(width=50, corner_radius=5, fg_color='#393e46', hover_color='#222831')
+        self.configure(width=50, corner_radius=5, fg_color=('gray', '#393e46'), hover_color=('dark gray', '#222831'))
         self.configure(**kwargs)
