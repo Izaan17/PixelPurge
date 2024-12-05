cbd4002fdf341affff6c9755d9a4a25ebf8a9108
izaan noman <izaan.nyc@gmail.com>
2024-12-04 23:49:23 -0500

    Add check_box.py

---
+++ widgets/checkboxes/check_box.py
@@ -0,0 +1,8 @@
+import customtkinter
+
+class DefaultCheckBox(customtkinter.CTkCheckBox):
+    def __init__(self, master: any, **kwargs):
+        super().__init__(master, **kwargs)
+        self.configure(fg_color='#2563EB', hover_color='#1D4ED8')
+        # Allow color overriding
+        self.configure(**kwargs)
