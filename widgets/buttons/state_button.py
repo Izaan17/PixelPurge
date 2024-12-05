b8983e4d874c4821f67639505a55d0ea96b12d34
izaan.noman <izaan.nyc@gmail.com>
2024-12-04 23:56:08 -0500

    Add doc string

--- widgets/buttons/state_button.py
+++ widgets/buttons/state_button.py
@@ -4,6 +4,12 @@ import customtkinter
 
 class StateButton(customtkinter.CTkButton):
     def __init__(self, master: any, initial_state: Literal[0, 1] = 0, **kwargs):
+        """
+        A customtkinter button with flexible state management.
+        :param master: The parent to attach to.
+        :param initial_state: The initial state to be set to.
+        :param kwargs: Keyword args for button.
+        """
         # Initialize the state (0 or 1)
         super().__init__(master, **kwargs)
         self.current_state = initial_state
