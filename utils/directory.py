bcd6a3fe48a5b313c72c01b165808df9a5fb17a1
izaan.noman <izaan.nyc@gmail.com>
2024-12-09 16:50:44 -0500

    Add absolute directories

---
+++ utils/directory.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+
+def get_project_root():
+    """Get the path of the project master directory."""
+    return Path(__file__).parent.parent\
+
+def get_app_dir():
+    app_dir = Path.home() / 'PixelPurge'
+    return app_dir
+
+def get_icon_dir():
+    return get_project_root() / 'icons'
+
+print(get_project_root())
